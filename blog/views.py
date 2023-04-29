a40314df829c18b5b45bd9d4c1886e44a5ed7d35
jh31barbosa <vazavult@gmail.com>
2023-04-29 18:29:39 -0300

    add more features like edit and delete posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
 class BlogDetailView(DetailView):
@@ -8,3 +11,18 @@ class BlogDetailView(DetailView):
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title","author","body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title","body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
